478799a82dc50f5bef0d58b10ff487bbbd7b6ff9
dyckdavid <dyckd27@gmail.com>
2023-08-30 17:13:20 -0600

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,4 +6,5 @@
 <section class="pt-8">
     <h1 class="text-5xl text-center">Welcome to Arco Implements</h1>
     <p class="mt-4 text-xl text-center">This website is currently under construction, but it will be available soon.</p>
+    
 </section>

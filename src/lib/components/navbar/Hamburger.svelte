5cd57dba39de8f51d01b518933e7071a7124fff5
dtremp007 <104601412+dtremp007@users.noreply.github.com>
2023-09-01 18:36:12 -0600

    Rewrote Navbar (#15)

---
+++ src/lib/components/navbar/Hamburger.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "./Hamburger.css";
+</script>
+
+<div class="cj-menuicon">
+  <label class="cj-menuicon-label" for="cj-menustate">
+    <span class="cj-menuicon-bread cj-menuicon-bread-top">
+      <span class="cj-menuicon-bread-crust cj-menuicon-bread-crust-top" />
+    </span>
+    <span class="cj-menuicon-bread cj-menuicon-bread-bottom">
+      <span class="cj-menuicon-bread-crust cj-menuicon-bread-crust-bottom" />
+    </span>
+  </label>
+</div>

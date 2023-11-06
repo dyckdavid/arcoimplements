e07c00e090c10f4737e9120f6eb2d6a0861fed20
dyckdavid <dyckd27@gmail.com>
2023-11-06 15:52:42 -0600

    Fixed Style issue Close #16

--- src/lib/components/Slider.svelte
+++ src/lib/components/Slider.svelte
@@ -1,92 +1,116 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
+  import { writable } from 'svelte/store';
 
-  let images: string[] = [
+  const slides = [
     'https://firebasestorage.googleapis.com/v0/b/arco-implements.appspot.com/o/about%20img%2Fappletrailer.jpg?alt=media&token=656b8f71-305a-4b83-9525-7e78f4e290d4',
     'https://firebasestorage.googleapis.com/v0/b/arco-implements.appspot.com/o/about%20img%2Fbalescooper.png?alt=media&token=1fbad91b-69a1-4629-a273-7541f391c2f1',
     'https://firebasestorage.googleapis.com/v0/b/arco-implements.appspot.com/o/about%20img%2Fworkers.JPG?alt=media&token=8dce517c-9691-4ca9-b9e6-c24bb10d3eba',
-    'https://firebasestorage.googleapis.com/v0/b/arco-implements.appspot.com/o/about%20img%2Fbalescooper.png?alt=media&token=1fbad91b-69a1-4629-a273-7541f391c2f1',
-    'https://firebasestorage.googleapis.com/v0/b/arco-implements.appspot.com/o/about%20img%2Fworkers.JPG?alt=media&token=8dce517c-9691-4ca9-b9e6-c24bb10d3eba'
+    // ... other images
   ];
 
-  let currentIndex: number = 0;
-  let intervalId: number;
+  const currentSlide = writable(0);
+  let startX: number;
+  let isDown = false;
 
-  const nextImage = (): void => {
-    if(currentIndex < images.length - 1) {
-      currentIndex += 1;
-    } else {
-      currentIndex = 0;
-    }
-  };
+  $: transform = `translateX(-${$currentSlide * 100}%)`;
 
-  const selectImage = (i: number): void => {
-    currentIndex = i;
-  };
-
-  onMount(() => {
-    if (typeof window !== 'undefined') {
-      intervalId = window.setInterval(nextImage, 3000); // Change every 3 seconds
-    }
-  });
-
-  onDestroy(() => {
-  if (intervalId !== undefined) {
-    window.clearInterval(intervalId);
+  function handleMousedown(event: MouseEvent) {
+    isDown = true;
+    startX = event.pageX;
   }
-});
 
-</script>
-
-  
-  <style>
-    .image-container {
-      position: relative;
-      width: 100%;
-      max-width: 500px;
-      height: 300px;
-    }
-  
-    .image-container img {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      transition: opacity 1s;
-      opacity: 0;
-    }
-  
-    .image-container img.active {
-      opacity: 1;
-    }
-  
-    @media (max-width: 768px) {
-      .image-container {
-        width: calc(100vw - 50px); /* Full viewport width with 50px margin on each side */
-        height: calc((100vw - 50px) * 0.75); /* Scale height with width */
-        max-width: none;
-        margin-left: 0px;
-        margin-right: 0px;
+  function handleMousemove(event: MouseEvent) {
+    if (isDown) {
+      const x = event.pageX;
+      const walk = (x - startX) * 3; // scroll-fast
+      if (walk > window.innerWidth / 4) {
+        prevSlide();
+        isDown = false;
+      } else if (walk < -window.innerWidth / 4) {
+        nextSlide();
+        isDown = false;
       }
     }
-  </style>
-  
-  <div class="flex flex-col items-center justify-center">
-    <div class="image-container">
-      {#each images as image, i (i)}
-        <img class:active={i === currentIndex} src={image} alt={`Image ${i}`} />
-      {/each}
-    </div>
-    <div class="mt-4 flex justify-center space-x-2">
-      {#each images as _, i (i)}
-        <button class="h-4 w-4 rounded-full bg-blue-500 focus:outline-none"
-                class:opacity-50={currentIndex !== i}
-                on:click={() => { selectImage(i); }}></button>
-      {/each}
-    </div>
+  }
+
+  function handleMouseup() {
+    isDown = false;
+  }
+
+  function nextSlide() {
+    currentSlide.update(n => (n + 1) % slides.length);
+  }
+
+  function prevSlide() {
+    currentSlide.update(n => (n - 1 + slides.length) % slides.length);
+  }
+</script>
+
+<style>
+  .slider-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .slider {
+    display: flex;
+    overflow: hidden;
+    cursor: grab;
+    max-width: 500px; /* Set a max-width for the slider */
+    margin-bottom: 10px; /* Space between slider and buttons */
+  }
+
+  .slider:active {
+    cursor: grabbing;
+  }
+
+  .slide {
+    min-width: 100%;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  img {
+    width: 100%;
+    display: block;
+  }
+
+  .navigation {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+  }
+
+  button {
+    padding: 5px 10px;
+    background-color: #f0f0f0;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+
+  button:hover {
+    background-color: #e0e0e0;
+  }
+</style>
+
+<div class="slider-container">
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div class="slider"
+       on:mousedown={handleMousedown}
+       on:mouseup={handleMouseup}
+       on:mouseleave={handleMouseup}
+       on:mousemove={handleMousemove}>
+    {#each slides as slide, index (slide)}
+      <div class="slide" style="transform: {transform}">
+        <img src={slide} alt={`Slide ${index + 1}`} />
+      </div>
+    {/each}
   </div>
-  
-  
-  
+  <div class="navigation">
+    <button on:click={prevSlide}>Previous</button>
+    <button on:click={nextSlide}>Next</button>
+  </div>
+</div>

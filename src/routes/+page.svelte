a1a1d1b9ccd1757cc0879ddec30b54660618aa7f
dyckdavid <dyckd27@gmail.com>
2023-11-06 16:39:44 -0600

    made basic contact page #8

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Arco Implements designed by David Dyck" />
-</svelte:head>
-
-<section class="pt-8">
+<script lang="ts">
+    // Import the icon component from your icon library setup
+    import { IconPhone } from '@tabler/icons-svelte';
+  </script>
+  
+  <svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Arco Implements designed by David Dyck" />
+  </svelte:head>
+  
+  <section class="pt-8">
     <h1 class="text-5xl text-center">Welcome to Arco Implements</h1>
     <p class="mt-4 text-xl text-center">This website is currently under construction, but it will be available soon.</p>
     
-</section>
+    <!-- Centered button with Tabler icon -->
+    <div class="flex justify-center mt-8">
+      <a href="/contact" class="flex items-center text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 md:text-lg lg:px-10 lg:py-4">
+        <!-- Tabler icon as a component -->
+        <IconPhone class="mr-2" />
+        Contact Us
+      </a>
+    </div>
+  </section>
+  
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    // TypeScript script can be included if necessary.
+  </script>
+  
+  <div class="flex flex-col items-center justify-center w-full min-h-screen px-4">
+    <!-- Dark section for contact details -->
+    <div class="bg-gray-800 text-white shadow-lg rounded-lg p-8 mb-8 max-w-4xl mx-auto w-full">
+      <div class="mb-6 text-center sm:text-left">
+        <h1 class="text-3xl font-bold mb-4">Contact Us</h1>
+        <p class="text-xl">Reach out via phone or email.</p>
+      </div>
+      <div class="flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
+        <div class="mb-6 sm:mb-0">
+          <h2 class="text-xl font-bold mb-2">Phone</h2>
+          <p>+52 625 594 3429</p>
+        </div>
+        <div>
+          <h2 class="text-xl font-bold mb-2">Email</h2>
+          <p>estufaselarco@gmail.com</p>
+        </div>
+      </div>
+    </div>
+    <!-- Light section for maps iframe -->
+    <div class="shadow-lg rounded-lg overflow-hidden max-w-4xl mx-auto w-full">
+      <div class="relative" style="height: 0; padding-bottom: 56.25%;">
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <iframe
+          class="absolute top-0 left-0 w-full h-full"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22270.87552797161!2d-106.85868869708742!3d28.466205065926044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86c1c9267981e2c3%3A0xf4f6dd51f411e669!2sEstufas%20El%20Arco!5e0!3m2!1sen!2smx!4v1699309386795!5m2!1sen!2smx"
+          style="border:0;"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  </div>
+  

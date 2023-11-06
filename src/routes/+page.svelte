1824c33f405a34ac8deb6eeef203221eb046d957
dyckdavid <dyckd27@gmail.com>
2023-11-06 16:40:19 -0600

    Added contact us button to home screen #3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,6 @@
     <!-- Centered button with Tabler icon -->
     <div class="flex justify-center mt-8">
       <a href="/contact" class="flex items-center text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 md:text-lg lg:px-10 lg:py-4">
-        <!-- Tabler icon as a component -->
         <IconPhone class="mr-2" />
         Contact Us
       </a>

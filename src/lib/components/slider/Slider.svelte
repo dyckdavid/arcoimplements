ddaac47fb499ffa790391dbe0f3956a6285decd2
dyckdavid <dyckd27@gmail.com>
2023-11-07 18:06:15 -0600

    Left and Right Arrow update #9

--- src/lib/components/slider/Slider.svelte
+++ src/lib/components/slider/Slider.svelte
@@ -2,8 +2,8 @@
 	import embla from './index';
 	import type { EmblaCarouselType } from 'embla-carousel';
 	import { writable } from 'svelte/store';
-  import { IconArrowNarrowRight } from '@tabler/icons-svelte';
-  import { IconArrowNarrowLeft } from '@tabler/icons-svelte';
+  import { IconChevronCompactRight } from '@tabler/icons-svelte';
+  import { IconChevronCompactLeft } from '@tabler/icons-svelte';
 
 	const images = Array.from({ length: 3 }, (_, index) => `/slide-${(index % 4) + 1}.jpg`);
 	const carousel = writable<EmblaCarouselType>();
@@ -21,7 +21,7 @@
 
 <div class="relative bg-embla-carousel p-4 overflow-hidden rounded-[.25rem]">
 	<div class="overflow-hidden" use:embla={{ store: carousel }} on:e-select={onSelect}>
-		<div class="grid grid-flow-col auto-cols-[100%] grid-rows-[190px] gap-x-[10px]">
+		<div class="grid grid-flow-col auto-cols-[100%] grid-rows-[320px] gap-x-[10px]">
 			{#each images as src, index}
 				<div class="relative overflow-hidden rounded-[.25rem]">
 					<img class="w-full h-full object-cover" {src} alt="" />
@@ -35,14 +35,14 @@
 		disabled={selected === 0}
 		on:click={onLeft}
 	>
-		<IconArrowNarrowLeft />
+		<IconChevronCompactLeft class="w-9 h-9" />
 	</button>
 	<button
 		class="absolute top-1/2 right-4 disabled:opacity-30 disabled:cursor-not-allowed"
 		disabled={selected === images.length - 1}
 		on:click={onRight}
 	>
-  <IconArrowNarrowRight />
+  <IconChevronCompactRight />
 	</button>
 
 	<div class="absolute bottom-4 left-0 w-full flex justify-center gap-4">
@@ -56,3 +56,12 @@
 		{/each}
 	</div>
 </div>
+
+<style>
+  /* Ensure your icon scales as needed and is aligned properly */
+  :global(svg.tabler-icon) {
+    width: 2.5rem;
+    height: 2.5rem;
+    align-items: center;
+  }
+</style>

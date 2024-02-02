44e246ba74726d6950a5c1bb276e8ca1ee209d64
dyckdavid <dyckd27@gmail.com>
2024-02-02 17:21:10 -0600

    Icon change

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -187,7 +187,6 @@
         <div class="section3 content-section content-center">
           <div class="flex justify-center mt-8 text">
             <a use:scrollFade href="/products" class="flex items-center text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 md:text-lg lg:px-10 lg:py-4 transition-opacity duration-1000 ease-in-out opacity-0">
-              <IconShoppingBag class="mr-2" />
             About
             </a>
           </div>

9f6c9005fd9e54a6324a58ec7cb295a013d2ab5b
David Rempel <ddrempti@gmail.com>
2024-03-08 11:30:57 -0600

    Prerender the website

--- src/routes/products/[id]/+page.server.ts
+++ src/routes/products/[id]/+page.server.ts
@@ -1,6 +1,15 @@
 import { getProductById, getProducts } from "$lib/data/products";
 import type { PageServerLoad } from "./$types";
 
+export async function entries() {
+	const products = await getProducts();
+
+	return products.map((product) => ({
+		id: product._id,
+	}));
+}
+
+
 export const load: PageServerLoad = async ({ params }) => {
   try {
     const product = await getProductById(params.id);

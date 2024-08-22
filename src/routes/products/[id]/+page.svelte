b86fa1424a90de019b3a4c533af0f21ff9b6e02e
David Dyck <dyckd27@gmail.com>
2024-08-22 17:30:41 -0600

    Started working on new details page UI got some styling still many things to add and not working properly yet

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -1,81 +1,33 @@
-<script lang="ts" context="module">
-  export interface MediaItem {
-    src: string;
-    type: 'image' | 'video';
-    title: string;
-  }
-</script>
-
 <script lang="ts">
-  import { PortableText } from "@portabletext/svelte";
-  import type { PageData } from "./$types";
-  import Carousel2 from "$lib/components/imagecarousel/Carousel2.svelte";
-  import { writable } from 'svelte/store';
-
-  export let data: PageData;
-  let product = data.product;
-
-  const transformMediaUrls = (urls: string[]): MediaItem[] => {
-    return urls.map(url => ({
-      src: url,
-      type: url.match(/\.(mp4|mov|wmv)$/) ? 'video' : 'image' as 'video' | 'image',
-      title: 'A descriptive title'
-    }));
-  };
-
-  let mediaItems: MediaItem[] = product ? transformMediaUrls(product.imageUrls) : [];
-
-  export const showModal = writable(false);
-
-  // const openModal = () => {
-  //   showModal.set(true);
-  // };
-
-  // const closeModal = () => {
-  //   showModal.set(false);
-  // };
+  
 </script>
 
-{#if product}
-<div class="flex flex-col md:flex-row h-screen">
-    <!-- Large Image Section -->
-    <div class="flex-shrink-0 md:flex-3 bg-gray-200 flex items-center justify-center w-full md:w-3/4 fixed md:static h-screen">
-      <div class="w-full h-full object-cover">
-        <Carousel2 images={mediaItems} />
+<div class="fixed top-0 left-0 h-full w-1/2 " aria-hidden="true"></div>
+<div class="fixed top-0 right-0 h-full w-1/2 " aria-hidden="true"></div>
+<div class="relative grid grid-cols-[2rem_1fr_2rem] xl:grid-cols-[minmax(2rem,1fr)_16rem_minmax(10
+00px,calc(80rem-32rem))_16rem_minmax(2rem,1fr)] lg:grid-cols-[2rem_minmax(200px,calc(50%-0rem))_16rem_2rem]">
+
+<div class="max-h-screen xl:sticky lg:static top-12 col-[2] row-[2] border-b border-gray-200  xl:border-b-0 xl:border-r xl:border-gray-200">
+  <div class="h-full py-6 pl-4 pr-6 sm:pl-6 lg:pl-8 xl:pl-0">
+    <!-- Start left column area -->
+    <div class="relative h-full" style="min-height: 12rem">
+      <div class="absolute inset-0 rounded-lg border-2 border-dashed border-gray-200 flex justify-center items-center">
+        Aside
       </div>
     </div>
-  
-    <!-- Scrollable Content Section -->
-    <div class="flex-1 h-screen overflow-y-auto p-4 ml-auto md:w-1/4">
-      <h1 class="text-2xl font-bold">Scrollable Text Section</h1>
-      <p class="mt-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
-      </p>
-      <p class="mt-4">
-        Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-      </p>
-      <p class="mt-4">
-        Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit.
-      </p>
-      <p class="mt-4">
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices.
-      </p>
-      <p class="mt-4">
-        Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.
-      </p>
-      <p class="mt-4">
-        Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit.
-      </p>
-      <p class="mt-4">
-        Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit.
-      </p>
-      <p class="mt-4">
-        Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit.
-      </p>
-    </div>
+    <!-- End left column area -->
   </div>
-{:else}
-  <p class="pt-20">Could not find product.</p>
-{/if}
+</div>
 
-
+<div class=" col-[2] row[3] xl:col-[3] xl:row-[2] min-h-[150vh] w-1/2">
+  <div class="h-full py-6 px-4 sm:px-6 lg:px-8">
+    <!-- Start main area-->
+    <div class="relative h-full" style="min-height: 36rem">
+      <div class="absolute inset-0 rounded-lg border-2 border-dashed border-gray-200 flex justify-center items-center">
+        Main
+      </div>
+    </div>
+    <!-- End main area -->
+  </div>
+</div>
+</div>
